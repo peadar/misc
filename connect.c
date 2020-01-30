d2b99d2035fecfc2f3f6845bbe0fd7e4cbfc0a35
Peter Edwards <peadar@arista.com>
2020-01-30 12:28:14 +0000

    Add stuff to connect

--- connect.c
+++ connect.c
@@ -1,53 +1,124 @@
 #define _POSIX_SOURCE
 #define _GNU_SOURCE
 #include <assert.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
+void
+usage()
+{
+    fprintf(stderr, "usage: connect -h <host> -p <port> -c -l <chunk>\n");
+    exit(-1);
+}
+
+int
+expand_crlf(const char *in, char *out, size_t len)
+{
+    const char *p;
+    char *q = out;
+    for (p = in; p != in + len;) {
+        if (*p == '\n')
+            *q++ = '\r';
+        *q++ = *p++;
+    }
+    return q - out;
+}
 
 int
 main(int argc, char *argv[])
 {
     int rc, addrcount, sock;
-    struct sockaddr_storage storage;
     struct addrinfo *ai, *aip, hints;
+    const char *host = 0;
+    const char *port = 0;
+    int c;
+    int chunklen = 3;
+    bool crlf = false;
 
-    if (argc != 3) {
-        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
-        exit(1);
+    while ((c = getopt(argc, argv, "h:p:l:c")) != -1) {
+        switch (c) {
+            case 'h':
+                host = optarg;
+                break;
+            case 'p':
+                port = optarg;
+                break;
+            case 'l':
+                chunklen = atoi(optarg);
+                break;
+            case 'c':
+                crlf = true;
+                break;
+            default:
+                usage();
+        }
     }
+    if (host == 0 || port == 0)
+        usage();
+    char *in = malloc(chunklen);
+    char *out = malloc(chunklen * 2);
+
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET; /* just get IPv4, which we know is smaller than storage. */
     hints.ai_socktype = SOCK_STREAM;
 
-    rc = getaddrinfo(argv[1], argv[2], &hints, &ai);
+    rc = getaddrinfo(host, port, &hints, &ai);
 
     if (rc != 0) {
-        fprintf(stderr, "can't resolve address %s:%s : %s\n", argv[1], argv[2], gai_strerror(rc));
+        fprintf(stderr, "can't resolve address %s:%s : %s\n", host, port, gai_strerror(rc));
         exit(1);
     }
 
     for (aip = ai, addrcount = 0; aip; aip = aip->ai_next, ++addrcount) {
-        assert(aip->ai_addrlen <= sizeof storage);
-        memset(&storage, 0, sizeof storage);
-        memcpy(&storage, aip->ai_addr, aip->ai_addrlen);
         sock = socket(aip->ai_family, aip->ai_socktype, aip->ai_protocol);
         if (sock == -1) {
             fprintf(stderr, "can't connect to address %d\n", addrcount);
             continue;
         }
-        rc = connect(sock, (const struct sockaddr *)&storage, sizeof storage);
-        if (rc == 0)
-            printf("connect: success\n");
-        else
-            printf("connect: fail: %s\n", strerror(errno));
+
+        rc = connect(sock, ai->ai_addr, ai->ai_addrlen);
+        if (rc != 0) {
+            fprintf(stderr, "connect: fail: %s\n", strerror(errno));
+            close(sock);
+            continue;
+        }
+        printf("connected\n");
+        if (fork()) {
+            for (;;) {
+                rc = read(sock, in, chunklen);
+                if (rc <= 0)
+                    _exit(0);
+                printf("<{%.*s}\n", rc, in);
+            }
+        }
+        for (;;) {
+            ssize_t rc = read(0, in, chunklen);
+            if (rc <= 0)
+                break;
+            char *w;
+            if (crlf) {
+                rc = expand_crlf(in, out,  rc);
+                w = out;
+            } else {
+                w = in;
+            }
+            write(sock, w, rc);
+            printf(">{%.*s}\n", (int)rc, w);
+            usleep(100000);
+        }
+        wait(&rc);
         close(sock);
     }
     freeaddrinfo(ai);
