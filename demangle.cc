ead177f27d1c3cc09844873879609d98c960d95a
Peter Edwards <peadar@arista.com>
2023-08-26 14:02:58 +0100

    better diags

--- demangle.cc
+++ demangle.cc
@@ -1,5 +1,13 @@
 #include <cxxabi.h>
 #include <iostream>
+#include <map>
+
+static std::map<int, std::string> errors = {
+   { 0,  "The demangling operation succeeded" },
+   { -1, "A memory allocation failure occurred" },
+   { -2, "mangled_name is not a valid name under the C++ ABI mangling rules." },
+   { -3, "One of the arguments is invalid." }
+};
 
 int
 main(int argc, char *argv[])
@@ -12,6 +20,14 @@ main(int argc, char *argv[])
         buf = __cxxabiv1::__cxa_demangle(argv[i], buf, &size, &status);
         if (status == 0) {
             std::cout << buf << std::endl;
+        } else {
+           std::cerr << "failed to demangle: ";
+           auto it = errors.find(status);
+           if (it != errors.end())
+              std::cerr << it->second;
+           else
+              std::cerr << status;
+           std::clog << std::endl;
         }
     }
     free(buf);
